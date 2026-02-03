0c419100ab9c58cafe0aac242a3969e2fadc7167
Sanggeun Ji <sanggeunji0117@gmail.com>
2026-02-03 19:16:23 +0900

    refactor: 1934 Confirmation Rate

--- LeetCode/SQL/1934_leetcode.sql
+++ LeetCode/SQL/1934_leetcode.sql
@@ -15,9 +15,26 @@ FROM (
     RIGHT JOIN Confirmations C 
         ON S.user_id = C.user_id
 ) t1 
-GROUP BY t1.user_id
+GROUP BY t1.user_id;
 
 -- 회고
 -- 1. FULL OUTER JOIN의 경우 MYSQL은 LEFT RIGHT JOIN으로 구현
 -- 2. CASE WHEN문 사용해 action이 confirmed일 때만 1로 나머지 상태는 0으로 나오게 설정 (if-else와 동일)
 -- 3. table끼리 합칠 때, id가 같으면 나중에 select할 때, 모호해지므로, AS로 이름 바꿔서 예방
+-- 4. 개선점: SUM(t1.is_confirmed)/COUNT(t1.is_confirmed) 대신 AVG(t1.is_confirmed)로 할 수 있음.
+
+SELECT t1.user_id, ROUND(COUNT(t1.is_confirmed), 2) AS confirmation_rate 
+FROM (
+    SELECT S.user_id, S.time_stamp, C.user_id AS C_user_id, C.time_stamp AS C_time_stamp, C.action, CASE WHEN action = "confirmed" THEN 1 ELSE 0 END AS is_confirmed 
+    FROM Signups S 
+    LEFT JOIN Confirmations C 
+        ON S.user_id = C.user_id
+
+    UNION
+
+    SELECT S.user_id, S.time_stamp, C.user_id AS C_user_id, C.time_stamp AS C_time_stamp, C.action, CASE WHEN action = "confirmed" THEN 1 ELSE 0 END AS is_confirmed 
+    FROM Signups S 
+    RIGHT JOIN Confirmations C 
+        ON S.user_id = C.user_id
+) t1 
+GROUP BY t1.user_id;
